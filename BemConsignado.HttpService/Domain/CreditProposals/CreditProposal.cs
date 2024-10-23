98594892bea7b333e0db474a2b3fef23b5f13f5a
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2024-10-23 13:19:30 -0300

    adição de validações

--- BemConsignado.HttpService/Domain/CreditProposals/CreditProposal.cs
+++ BemConsignado.HttpService/Domain/CreditProposals/CreditProposal.cs
@@ -1,20 +1,34 @@
 ﻿using BemConsignado.HttpService.Domain.Proponents;
+using CSharpFunctionalExtensions;
 
 namespace BemConsignado.HttpService.Domain.CreditProposals
 {
     public class CreditProposal
     {
         public int Id { get; set; }
-        public ProposalStatus Status { get; private set; }
+        public CreditProposalStatus Status { get; private set; }
         public Proponent Proponent { get; private set; }
         public int Installments { get; private set; }
         public decimal Credit { get; private set; }
 
-        public static CreditProposal Create(Proponent proponent, decimal credit, int installments)
+        public static Result<CreditProposal> Create(Proponent proponent, decimal credit, int installments)
         {
+            var hasOpenProposal = proponent.Proposals.Any(p => p.Status == CreditProposalStatus.Open);
+            if (hasOpenProposal)
+                return Result.Failure<CreditProposal>("Proponente já contém uma proposta de crédito consignado aberta.");
+
+            if (!proponent.IsActive)
+                return Result.Failure<CreditProposal>("Proponente deve estar ativo.");
+
+            int maxPaymentYear = 80;
+            DateTime maxPaymentDate = proponent.BirthDate.AddYears(maxPaymentYear);
+            DateTime lastInstallmentDate = DateTime.Now.AddMonths(installments);
+            if (lastInstallmentDate > maxPaymentDate)
+                return Result.Failure<CreditProposal>("A Última parcela de pagamento não pode exceder a idade de 80 anos do proponente.");
+
             return new CreditProposal
             {
-                Status = ProposalStatus.Open,
+                Status = CreditProposalStatus.Open,
                 Proponent = proponent,
                 Installments = installments,
                 Credit = credit
@@ -22,7 +36,7 @@ namespace BemConsignado.HttpService.Domain.CreditProposals
         }
     }
 
-    public enum ProposalStatus
+    public enum CreditProposalStatus
     {
         Closed = 0,
         Open = 1
--- BemConsignado.HttpService/Domain/CreditProposals/Handlers/CreateCreditProposalHandler.cs
+++ BemConsignado.HttpService/Domain/CreditProposals/Handlers/CreateCreditProposalHandler.cs
@@ -6,16 +6,16 @@ namespace BemConsignado.HttpService.Domain.CreditProposals
 {
     public class CreateCreditProposalHandler(ProponentRepository proponentRepository, CreditProposalRepository creditProposalRepository) : IRequestHandler<CreateCreditProposalCommand, Result<CreditProposal>>
     {
-        public async Task<Result<CreditProposal>> Handle(CreateCreditProposalCommand request, CancellationToken cancellationToken)
+        public async Task<Result<CreditProposal>> Handle(CreateCreditProposalCommand request)
         {
             var proponent = await proponentRepository.GetAsync(request.Cpf);
             if(proponent == null)
                 return Result.Failure<CreditProposal>($"Proponente não foi encontrado com esse cpf: '{request.Cpf}'");
 
             var creditProposal = CreditProposal.Create(proponent, request.Credit, request.Installments);
-            await creditProposalRepository.AddAsync(creditProposal);
+            await creditProposalRepository.AddAsync(creditProposal.Value);
             await creditProposalRepository.UnitOfWork.SaveChangesAsync();
-            return Result.Success(creditProposal);
+            return creditProposal;
         }
     }
 }
